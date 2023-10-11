fbb3f9c45084f7c73515337242a189401028fa81
Sebastian <sebastian.gonzalez.cass@usach.cl>
2023-10-11 16:18:34 -0300

    fix: empty file

--- lectura.c
+++ lectura.c
@@ -19,12 +19,26 @@ Celda *leer_archivo(char *nombre_archivo)
         printf("Error: Archivo no encontrado\n");
         exit(1);
     }
+    // Moverse al final del archivo
+    fseek(archivo, 0, SEEK_END);
+
+    // Obtener la posición actual (tamaño del archivo en bytes)
+    long tamaño = ftell(archivo);
+
+    if (tamaño == 0)
+    {
+        printf("El archivo está vacío.\n");
+        exit(1);
+    }
+
+    // Volver al inicio del archivo
+    fseek(archivo, 0, SEEK_SET);
     // Se lee la primera linea
     char linea[100];
     int tamaño_array = atoi(fgets(linea, 100, archivo));
     if (tamaño_array <= 0)
     {
-        printf("Error: Archivo vacío o No Cumple con el Formato Pedido\n");
+        printf("Error: No Cumple con el Formato Pedido\n");
         exit(1);
     }
     // Se crea un array de array de caracteres con el tamaño de la primera linea
